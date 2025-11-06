2ee16e0a336b63223e81d0975e20e59a74458bda
ItsHeroPH <heroreyes1973@gmail.com>
2025-11-06 09:56:33 +0800

    few changes

--- frontend/src/components/ChatBubble.jsx
+++ frontend/src/components/ChatBubble.jsx
@@ -44,8 +44,8 @@ export default function ChatBubble({ user, data, isOwner }) {
                     <img className="min-w-10 w-10 rounded-full" src={profile}/>
                     <div className="w-full h-fit flex flex-col items-start gap-1">
                         <p className="text-sm text-citrus-orange font-medium">{user.username}<span className="hidden md:inline"> • {dateString}</span></p>
-                        <div className="bg-slate-200 max-w-[66%] h-fit p-2 rounded-lg shadow-lg">
-                            <p className="text-md text-citrus-orange font-medium text-wrap text-start">{data.message}</p>
+                        <div className="bg-slate-200 max-w-[66%] h-fit p-2 rounded-lg shadow-lg breaks-words">
+                            <p className="text-md text-citrus-orange font-medium  break-all text-start">{data.message}</p>
                         </div>
                     </div>
                 </div>
