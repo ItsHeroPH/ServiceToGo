bd91f9257880043c28e5df134aa757ebbe8d3286
ItsHeroPH <heroreyes1973@gmail.com>
2025-11-07 00:22:06 +0800

    few changes

--- frontend/src/components/ChatBubble.jsx
+++ frontend/src/components/ChatBubble.jsx
@@ -30,7 +30,7 @@ export default function ChatBubble({ user, data, isOwner }) {
     return (
         <>
             { isOwner ? (
-                <div className="w-full h-fit flex items-end gap-2">
+                <div className="w-full h-fit flex items-start gap-2">
                     <div className="w-full h-fit flex flex-col items-end gap-1">
                         <p className="text-sm text-citrus-orange font-medium">You<span className="hidden md:inline"> • {dateString}</span></p>
                         <div className="bg-slate-200 max-w-[66%] h-fit p-2 rounded-lg shadow-lg breaks-words">
@@ -40,7 +40,7 @@ export default function ChatBubble({ user, data, isOwner }) {
                     <img className="min-w-10 w-10 rounded-full" src={profile}/>
                 </div>
             ) : (
-                <div className="w-full h-fit flex items-end gap-2">
+                <div className="w-full h-fit flex items-start gap-2">
                     <img className="min-w-10 w-10 rounded-full" src={profile}/>
                     <div className="w-full h-fit flex flex-col items-start gap-1">
                         <p className="text-sm text-citrus-orange font-medium">{user.username}<span className="hidden md:inline"> • {dateString}</span></p>
