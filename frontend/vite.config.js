655ee87f39d18627b748f204cbb27d646575fa47
ItsHeroPH <heroreyes1973@gmail.com>
2025-11-07 23:32:18 +0800

    obfuscator

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-import obfuscatorPlugin from "vite-plugin-obfuscator"
+import { viteObfuscateFile } from "vite-plugin-obfuscator"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
-      obfuscatorPlugin({
+      viteObfuscateFile({
         apply: "build",
         options: {
           compact: true,
