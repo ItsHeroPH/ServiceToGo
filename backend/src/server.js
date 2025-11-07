f096f002261bd1ccdbbbca7a2c9550d07219abaf
ItsHeroPH <heroreyes1973@gmail.com>
2025-11-08 03:11:22 +0800

    few changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -71,11 +71,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
-
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(204);
-    }
-
+    
     next();
   } catch (err) {
     return res.status(403).send("Invalid Request");
