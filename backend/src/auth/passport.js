b37c00757517c2da1a4e5595c9110651fd296518
ItsHeroPH <heroreyes1973@gmail.com>
2025-11-13 03:36:08 +0800

    bug fixes

--- backend/src/auth/passport.js
+++ backend/src/auth/passport.js
@@ -28,7 +28,7 @@ export function initializePassport(passport) {
     passport.deserializeUser(async (id, done) => {
         try {
             const user = await User.findOne({ id });
-            if(!user) done("User not found");
+            if(!user) done("User not found", false);
             const decryptedUser = {
                 id: user.id,
                 email: decrypt(user.email),
