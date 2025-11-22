858e43a110a6392d57b86e8905cf3153c78bed99
ItsHeroPH <heroreyes1973@gmail.com>
2025-11-22 22:27:21 +0800

    few fix

--- frontend/src/util/user.js
+++ frontend/src/util/user.js
@@ -1,15 +1,19 @@
 import api from "./api";
 
 export async function getUser() {
-    const response = await api.get("/user");
-    if(response.status === 200) {
-        if(response.user.avatar) {
-            const avatar = await api.get(`/images/${response.user.avatar}`, { responseType: "blob" });
-            return { ...response.user, avatar: URL.createObjectURL(avatar) }
+    try {
+        const response = await api.get("/user");
+        if(response.status === 200) {
+            if(response.user.avatar) {
+                const avatar = await api.get(`/images/${response.user.avatar}`, { responseType: "blob" });
+                return { ...response.user, avatar: URL.createObjectURL(avatar) }
+            } else {
+                return response.user
+            }
         } else {
-            return response.user
+            return null;
         }
-    } else {
+    } catch (err) {
         return null;
     }
 }
